72ba2a0f59c68ce8cae0d8a72185082735394701
Thad Kerosky <contact@thadk.net>
2021-01-29 19:52:13 -0500

    updated models.py to run database queries
    Co-authored-by: Rajinder Mavi <rsmavi.hb@gmail.com>
    Co-authored-by: ethan <ethanstrominger2@gmail.com>
    Co-authored-by: Terence Stone <terence.stone25@gmail.com>

--- controllers.py
+++ controllers.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, request, session
 from config import db
-#from models import *
+from models import *
 
 # https://flask-session.readthedocs.io/en/latest/
 # https://github.com/marcoagner/Flask-QRcode
---
+++ models.py
@@ -0,0 +1,43 @@
+#ORM models for State, Order, User and Log db
+from flask_sqlalchemy import sqlalchemy
+from flask_migrate import Migrate
+from sqlalchemy import func
+from config import *
+
+#id, State, Order Number, COffice, created_at , updated_at
+class Order(db.Model):
+    __tablename__="orders"
+    id=db.Column(db.Integer,primary_key=True)
+    state = db.Column(db.String(255))
+    order_number = db.Column(db.Integer,db.ForeignKey('order_number.id'),nullable = False)
+    coffice = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime,server_default = func.now())
+    updated_at = db.Column(db.DateTime,server_default = func.now())
+    
+#what actions do we need for ths table: Create and read
+    @classmethod
+    def new(state:str, order_number:int ,coffice:str):
+        self.state = state
+        self.order_number = order_number
+        self.coffice = coffice
+        new_order = self(state,order_number,coffice)
+        db.session.add(new_order)
+        db.session.commit()
+        return new_order
+
+    @classmethod
+    def get():
+        return self.query.all()
+
+    @classmethod
+    def getstate(state):
+        return self.query.filter(self.state == state)
+        
+    @classmethod
+    def getorder(order_number):
+        return self.query.filter(self.order_number == order_number)
+
+    @classmethod
+    def getcoffice(coffice):
+        return self.query.filter(self.coffice == coffice)
+
