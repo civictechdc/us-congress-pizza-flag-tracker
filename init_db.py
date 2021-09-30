98c9c7c92edc378ba37e27b430d38066d7eba2d3
Rajinder Mavi <rsmavi.hb@gmail.com>
2021-09-30 13:07:51 -0700

    Autopopulate status table.

--- init_db.py
+++ init_db.py
@@ -2,7 +2,7 @@ import click
 from flask.cli import with_appcontext
 from flask import current_app, g 
 from config import db
-from models import OfficeModel
+from models import OfficeModel, StatusModel
 import json
 # import uuid
 
@@ -13,16 +13,28 @@ def close_db(e=None):
         db.close()
 
 def init_db():
-    with open('office_codes.json',) as office_codes_json:
+    with open('home_office_codes.json',) as office_codes_json:
         office_codes_list = json.load(office_codes_json)
     
     for state_offices in office_codes_list:
         usa_state = state_offices["usa_state"]
-        for office_code in state_offices["office_code"]:
+        for office_code in state_offices["home_office_code"]:
             # TODO(tdk): we may not need uuids, discuss
             # theUuid = str(uuid.uuid4())
             office = OfficeModel(usa_state, office_code)
             db.session.add(office)
+
+    with open('statuses.json') as statuses_json:
+        statuses_list = json.load(statuses_json)
+
+    for status in statuses_list:
+        id = status["id"]
+        status_federal_office_code = status["status_federal_office_code"]
+        sequence_num = status["sequence_num"]
+        description = status["description"]
+        status = StatusModel(id,status_federal_office_code,sequence_num,description)
+        db.session.add(status)
+
     db.session.commit()
  
     
