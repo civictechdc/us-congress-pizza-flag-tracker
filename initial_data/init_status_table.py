d3e225f1ee9601d34a02a1bf13ec758b90e76b34
ethanstrominger <ethanstrominger2@gmail.com>
2021-11-23 07:00:06 -0500

    Remove initial data (replaced by data)

--- initial_data/init_status_table.py
+++
@@ -1,9 +0,0 @@
-def init_status_table(statuses_list,db):
-    from models import StatusModel
-    for status in statuses_list:
-        id = status["id"]
-        status_federal_office_code = status["status_federal_office_code"]
-        sequence_num = status["sequence_num"]
-        description = status["description"]
-        status = StatusModel(id,status_federal_office_code,sequence_num,description)
-        db.session.add(status)
