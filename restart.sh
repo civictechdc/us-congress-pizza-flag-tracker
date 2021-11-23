a9e22d88efd364c8f4a85969113cbe45c49758cb
ethanstrominger <ethanstrominger2@gmail.com>
2021-11-22 21:15:06 -0500

    Minor changes

--- restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-git rm -r migrations
-rm flag*.db
-DEBUG=True FLASK_ENV=development FLASK_APP=app.py flask db init
-DEBUG=True FLASK_ENV=development FLASK_APP=app.py flask db migrate
-DEBUG=True FLASK_ENV=development FLASK_APP=app.py flask db upgrade
-DEBUG=True FLASK_ENV=development FLASK_APP=app.py flask init-db
-cp flag.db flagtests.db
