c840c07af90e0cc509c534c3b57a2e9027d95798
ethanstrominger <ethanstrominger2@gmail.com>
2021-12-07 19:23:14 +0000

    gitpod init

---
+++ gitpod_init.sh
@@ -0,0 +1,6 @@
+echo 'init script' # runs during prebuild
+export PIP_USER=false
+cp .env.example .env
+pip3 install -r requirements.txt
+DEBUG=True FLASK_APP=app.py flask db upgrade
+python populate_seed_data.sql
