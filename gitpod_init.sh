083539bf9844f80e7a6d35667e15b9685490227b
ethanstrominger <ethanstrominger2@gmail.com>
2021-12-07 22:12:57 +0000

    gitpod init

--- gitpod_init.sh
+++ gitpod_init.sh
@@ -3,4 +3,4 @@ export PIP_USER=false
 cp .env.example .env
 pip3 install -r requirements.txt
 DEBUG=True FLASK_APP=app.py flask db upgrade
-python populate_seed_data.sql
+python populate_seed_data.py
