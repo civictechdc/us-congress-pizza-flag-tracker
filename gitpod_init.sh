f31d0c7701c29f437e5b95b0e96a559629b7de50
ethanstrominger <ethanstrominger2@gmail.com>
2021-12-08 22:22:38 +0000

    Add echo statements

--- gitpod_init.sh
+++ gitpod_init.sh
@@ -1,9 +1,11 @@
 echo 'init script' # runs during prebuild
 export PIP_USER=false
 cp .env.example .env
+echo Setting up myenv
 python -m venv myenv
 chmod 700 myenv/bin/*
 chmod 700 myenv/bin/*.*
+echo Activating 
 myenv/bin/activate
 pip3 install -r requirements.txt
 DEBUG=True FLASK_APP=app.py flask db upgrade
