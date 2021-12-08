0885544dceedfbff73f93178e8071adf116a06a5
ethanstrominger <ethanstrominger2@gmail.com>
2021-12-08 20:02:38 +0000

    Setup python env

--- gitpod_init.sh
+++ gitpod_init.sh
@@ -1,6 +1,10 @@
 echo 'init script' # runs during prebuild
 export PIP_USER=false
 cp .env.example .env
+python -m venv myenv
+chmod 700 myenv/bin/*
+chmod 700 myenv/bin/*.*
+myenv/bin/activate
 pip3 install -r requirements.txt
 DEBUG=True FLASK_APP=app.py flask db upgrade
 python populate_seed_data.py
