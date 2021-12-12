fe5b89df46489e938534c8b2142b4e8f02ce845a
ethanstrominger <ethanstrominger2@gmail.com>
2021-12-11 19:47:45 -0500

    Change pip command

--- refresh_pip_install2.sh
+++ refresh_pip_install2.sh
@@ -1,18 +1,17 @@
 #/bin/sh
-pyv="$(python3 -V 2>&1)"
 
 if [[ "$pyv" != *"command not found"* ]];
 then
   echo Running pip3 install
-  pycommand="python3"
+  pycommand="pip3"
 else
   echo Running pip install
-  pycommand="python"
+  pycommand="pip"
 fi
 
 echo NOTE: if this takes more than about 2 minutes, you can use alternative below. See https://stackoverflow.com/questions/65122957/resolving-new-pip-backtracking-runtime-issue for one article on the issue.
 echo .
 echo . pip install -r requirements.txt --use-deprecated=legacy-resolver
 echo .
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
 echo Done
