a0d903fecc3cb8fcd6eac3cccd707046793ecf57
Merritt Baggett <mertbagt@gmail.com>
2021-12-16 12:49:02 -0500

    added permissions to status tabel

--- models.py
+++ models.py
@@ -75,18 +75,19 @@ class StatusModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     sequence_num = db.Column(db.Integer)
     description = db.Column(db.String(255))
+    permission = db.Column(db.String(255))
     active_status = db.Column(db.String(255))
     status_code = db.Column(db.String(255))
     orders = db.relationship('OrderModel', back_populates="status")
-    # order_no = db.Column(db.Integer, db.ForeignKey('orders.order_number'))
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(
         db.DateTime, server_default=func.now(), onupdate=func.now())
 
-    def __init__(self, id, sequence_num, description, active_status, status_code):
+    def __init__(self, id, sequence_num, description, permission, active_status, status_code):
         self.id = id
         self.sequence_num = sequence_num
         self.description = description
+        self.permission = permission
         self.active_status = active_status
         self.status_code = status_code
 
