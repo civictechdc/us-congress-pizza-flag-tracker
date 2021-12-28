fbf55f7c2f5ef431164442675f68630d50ff3887
Terence Stone <terence.stone25@gtmail.com>
2021-12-28 17:07:46 -0500

    Completed log model and init function

--- models.py
+++ models.py
@@ -123,3 +123,25 @@ class UserModel(db.Model):
 
 #Log Table
 #id, order_number, home_office_code, status_code, active_status, status_description, order_updated_at
+
+class LogModel(db.Model):
+    __tablename__ = "logs"
+    id = db.Column(db.Integer, primary_key=True,nullable=False)
+    order_number = db.Column(db.Interger)
+    home_office_code=db.Column(db.String(10))
+    status_code = db.Column(db.String(255))
+    active_status = db.Column(db.String(255))
+    status_description = db.Column(db.String(255))
+    order_updated_at = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=func.now(), onupdate=func.now())
+
+    def __init__(self, id, order_number, home_office_code, status_code, active_status, status_description, order_updated_at):
+        self.id = id
+        self.order_number = order_number
+        self.home_office_code = home_office_code
+        self.status_code = status_code
+        self.active_status = active_status
+        self.status_description = status_description
+        self.order_updated_at = order_updated_at
