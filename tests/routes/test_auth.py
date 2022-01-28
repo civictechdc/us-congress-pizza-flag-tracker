f21fbee1109a71ddb7c44def9bbce20e0a4cb603
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2022-01-28 11:32:45 -0500

    fix minor test bugs

--- tests/routes/test_auth.py
+++ tests/routes/test_auth.py
@@ -3,6 +3,8 @@ from config import flask_app
 
 from src.auth.auth_controller import derive_token_from_username
 
+flask_app.testing = True
+
 
 class TestAuth:
     def test_orders(self):
@@ -10,19 +12,31 @@ class TestAuth:
         password = "FED-ADMIN-1010"
         token = derive_token_from_username(username)
         response = flask_app.test_client().get(
-            '/api/orders',
+            "/api/orders",
             headers={"x-access-tokens": token},
-            content_type='application/json',
+            content_type="application/json",
         )
         assert response.status_code == 200
 
     def test_if_not_is_admin_cannot_add_user(self):
-        username = "FED-AOC"
+        username = "MA-01"
         token = derive_token_from_username(username)
+        print("token:", token)
         response = flask_app.test_client().post(
-            '/api/users/create',
-            headers={"x-access-tokens": token},
-            content_type='application/json',
+            "/api/users/create",
+            headers={
+                "x-access-tokens": token,
+                "Content-type": "application/json",
+            },
+            json={
+                "username": "ASDFASDF001",
+                "password": "ASDFASDF",
+                "office_code": "MA-01",
+                "is_admin": "N",
+                "can_create_update_delete_orders": "N",
+                "can_update_password_for": "SELF",
+                "can_update_status_for": "MA-01",
+            },
         )
+        print("response:", response)
         assert response.status_code == 401
-
