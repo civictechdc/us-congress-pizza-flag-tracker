abc1cca06ff27038023a3c26a3cb317b51307eca
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2022-02-06 13:55:09 -0500

    update update password function

--- src/user/user_controller.py
+++ src/user/user_controller.py
@@ -1,7 +1,7 @@
-from flask import request
+from flask import request, Response
 from src.auth import auth_controller
 
-from src.auth.auth_controller import get_current_user, set_authorize_current_user 
+from src.auth.auth_controller import get_current_user, set_authorize_current_user
 from src.auth.auth_privileges import check_is_admin
 from src.user.user_actions import UserActions
 from src.user.user_model import UserParams
@@ -16,7 +16,9 @@ def create_user():
     userParams = UserParams()
     userParams.username = request_json["username"]
     userParams.password = request_json["password"]
-    userParams.can_create_update_delete_orders = request_json["can_create_update_delete_orders"]
+    userParams.can_create_update_delete_orders = request_json[
+        "can_create_update_delete_orders"
+    ]
     userParams.office_code = request_json["office_code"]
     userParams.can_update_password_for = request_json["can_update_password_for"]
     userParams.can_update_status_for = request_json["can_update_status_for"]
@@ -24,19 +26,29 @@ def create_user():
     new_user = UserActions.create(userParams)
     return table_record_to_json(new_user), 201
 
+
 def get_current_office():
     current_office = get_current_user().office_code
-    if 'FED' == current_office[:3]:
-        return 'FED'
+    if "FED" == current_office[:3]:
+        return "FED"
     else:
         return current_office
 
-#function for user to update password
-def self_update_password(new_password, old_password):
-    username = auth_controller.get_current_user().username
-    UserActions.self_update_password(username, new_password, old_password)
-    return username
+
+# function for user to update password
+def self_update_password():
+    request_json = request.get_json()["data"]
+    old_password = request_json["old_password"]
+    new_password = request_json["new_password"]
+
+    username = get_current_user().username
+
+    if UserActions.self_update_password(username, new_password, old_password):
+        return Response(response=username, status=200)
+    else:
+        return
+
 
 def admin_update_password(username, new_password):
     UserActions.admin_update_password(username, new_password)
-    return 
+    return
