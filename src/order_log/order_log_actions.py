40553704bb22c28c12627803b160cc68fabd2ea8
Terence Stone <terence.stone25@gtmail.com>
2022-04-05 11:14:34 -0400

    Added find max feature to get_order_log_uuid func.

--- src/order_log/order_log_actions.py
+++ src/order_log/order_log_actions.py
@@ -1,7 +1,4 @@
-# import re
 from src.order_log.order_log_model import OrderLogModel
-from src.status.status_model import StatusModel
-from src.status.status_actions import StatusActions
 from config import db
 import uuid
 
@@ -17,14 +14,33 @@ class LogActions:
         return new_log
 
     @classmethod
-    def get_order_log_uuid(cls, order_num):
-        order_logs = OrderLogModel.query.filter(OrderLogModel.order_number == order_num).all()
-        order_log_uuid = None
-        #sort this object
+    def get_order_log_uuid(cls, order_number):
+        order_logs = OrderLogModel.query.filter(OrderLogModel.order_number == order_number).all()
+        current_order_log_uuid = None
+        max_order_count = 0
+        order_logs_len = len(order_logs)
         for order in order_logs:
-            order_log_uuid = order.uuid
-        return order_log_uuid      
-        
+            if order_logs_len == 1:
+                current_order_log_uuid = order.uuid
+            elif order.order_status_id > max_order_count:
+                current_order_log_uuid = order.uuid
+        return current_order_log_uuid 
+
+    @classmethod
+    def sort_order_log_by_order_number(order_logs):
+        order_logs_len = len(order_logs)
+        for order in order_logs:
+            if order_logs_len == 1:
+                order_log_uuid = order.uuid
+            elif order.order_status_id > 0:
+                order #complete the sort
+        return order_log_uuid
+
+    @classmethod
+    def get_max_order_count(order_number):
+        #implemented to count the number of order_logs for an order
+        return
+
     @classmethod
     def get_all_order_logs(cls):
         order_logs = OrderLogModel.query.all()
@@ -34,4 +50,5 @@ class LogActions:
     def get_all_order_logs_by_order_number(cls, order_number):
         order_log_obj = OrderLogModel.query.get(order_number).all()
         return order_log_obj
+
         
--- src/order_log/order_log_model.py
+++ src/order_log/order_log_model.py
@@ -11,6 +11,7 @@ class OrderLogModel(db.Model):
     __tablename__ = "order_logs"
     uuid = db.Column(db.String(40), unique=True, index=True, primary_key=True, nullable=False)
     order_number = db.Column(db.Integer)
+    #order_count = db.Column(db.Interger)
     previous_order_log_uuid = db.Column(db.String(255), nullable = True)
     order_updated_at = db.Column(db.DateTime, server_default=func.now())
     order_uuid = db.Column(db.String(40), db.ForeignKey(OrderModel.uuid))
@@ -24,6 +25,7 @@ class OrderLogModel(db.Model):
     def __init__(self, logUuid, order_number, previous_order_log_uuid, order_uuid, usa_state, home_office_code, order_status_id):
         self.uuid = logUuid
         self.order_number = order_number
+        #self.order_count = order_count
         self.previous_order_log_uuid = previous_order_log_uuid
         self.order_uuid = order_uuid
         self.usa_state = usa_state 
