48454141f45d775e9eb31f57a1f7736d693b6de4
Terence Stone <terence.stone25@gtmail.com>
2022-05-25 13:06:21 -0400

    Updated LogActions func call in update_order func

--- src/order/order_actions.py
+++ src/order/order_actions.py
@@ -44,13 +44,15 @@ class OrderActions:
     def get_orders(cls, query_params: OrderQueryParams = OrderQueryParams()):
         query = OrderModel.home_office_code == OrderModel.home_office_code
         if query_params.office_code:
-            query = query & (OrderModel.home_office_code == query_params.office_code)
+            query = query & (OrderModel.home_office_code ==
+                             query_params.office_code)
         orders = OrderModel.query.filter(query)
         return [order for order in orders]
 
     @classmethod
     def get_order_by_order_number(cls, order_number):
-        order = OrderModel.query.filter(OrderModel.order_number == order_number).first()
+        order = OrderModel.query.filter(
+            OrderModel.order_number == order_number).first()
         return order
 
     @classmethod
@@ -88,7 +90,7 @@ class OrderActions:
         order.home_office_code = home_office_code or order.home_office_code
         order.order_status_id = order_status_id or order.order_status_id
         LogActions.create_order_log(
-            uuid, usa_state, order_number, home_office_code, order_status_id
+            uuid, order.usa_state, order.order_number, order.home_office_code, order.order_status_id
         )
         db.session.commit()
         return order
