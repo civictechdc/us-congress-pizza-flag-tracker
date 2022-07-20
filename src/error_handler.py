017484e82910cc938353a4c2d228985ff070a1c6
Merritt Baggett <mertbagt@gmail.com>
2022-07-20 14:07:34 -0400

    overhaul rough draft

--- src/error_handler.py
+++ src/error_handler.py
@@ -31,23 +31,21 @@ e.toString() (the string value of e).  See best practices for how to specify str
 @flask_app.errorhandler(Exception)
 def handle_exceptions_for_app(e: HTTPException):
     code = 500
-    response_var = None
-    if isinstance(e, HTTPException):
+    if e.code:
         code = e.code
-        response_var = e.response
-        if response_var:
-            response_var["error_msg"] = str(e)
-        else:
-            response_var = {"error_msg": str(e)}
-        print("Response", response_var)
+    response_var = None
+    if e.response:
+        response_var["error_msg"] = str(e)
+    else:
+        response_var = {"error_msg": str(e)}
     print("Code:", code)
+    print("Response Var: ", response_var)  
     print(traceback.print_exc())
-    print("Message (stored in http_response.response.body.error_msg):", str(e))
     now = datetime.now()
     text_msg = json.dumps(response_var)
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
     print()
     return flask_app.response_class(
-        response=text_msg, status=code, mimetype="application/json"
+        response=response_var.err_message, status=code, mimetype="application/json"
     )
